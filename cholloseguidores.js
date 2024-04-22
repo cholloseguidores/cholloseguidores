1463be6213290f4164487a3832714b55b8b3f559
cholloseguidores <167150600+cholloseguidores@users.noreply.github.com>
2024-04-22 06:23:20 +0200

    update

--- cholloseguidores.js
+++ cholloseguidores.js
@@ -1,13 +1,13 @@
 function openWhatsApp(packageName) {
-    const phoneNumber = "34672866006"; /* Verifica que este número es correcto */
+    const phoneNumber = "+34672866006";
     const message = encodeURIComponent("Hola, estoy interesado en adquirir " + packageName + " para mi cuenta de Instagram. ¿Podrías informarme por favor?");
     const url = "https://wa.me/" + phoneNumber + "?text=" + message;
-    window.open(url, "_blank"); /* Abre WhatsApp en una nueva pestaña */
+    window.open(url, "_blank"); /* Abre en una nueva pestaña */
 }
 
 function contactUs() {
-    const phoneNumber = "34672866006"; /* Verifica que este número es correcto */
+    const phoneNumber = "+34672866006";
     const message = encodeURIComponent("Hola, estoy interesado en trabajar con vosotros. ¿Podríais informarme más?");
     const url = "https://wa.me/" + phoneNumber + "?text=" + message;
-    window.open(url, "_blank"); /* Abre WhatsApp en una nueva pestaña */
+    window.open(url, "_blank"); /* Abre en una nueva pestaña */
 }
